a427a4eb815bc156ee08c7899457ddb4d2d88997
szapatao <szapatao@unal.edu.co>
2021-07-13 15:33:26 -0500

    Se añade form a login.js

--- src/Login.js
+++ src/Login.js
@@ -20,8 +20,69 @@ export default function Login() {
 
   return (
     <div>
+        <form>
+          {/* <!-- Email input --> */}
+          <div class="form-outline mb-4">
+            <input type="email" id="form2Example1" class="form-control" />
+            <label class="form-label" for="form2Example1">Email address</label>
+          </div>
+
+          {/* <!-- Password input --> */}
+          <div class="form-outline mb-4">
+            <input type="password" id="form2Example2" class="form-control" />
+            <label class="form-label" for="form2Example2">Password</label>
+          </div>
+
+          {/* <!-- 2 column grid layout for inline styling --> */}
+          <div class="row mb-4">
+            <div class="col d-flex justify-content-center">
+              {/* <!-- Checkbox --> */}
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="form2Example3"
+                  checked
+                />
+                <label class="form-check-label" for="form2Example3"> Remember me </label>
+              </div>
+            </div>
+
+            <div class="col">
+              {/* <!-- Simple link --> */}
+              <a href="#!">Forgot password?</a>
+            </div>
+          </div>
+
+          
+          
+
+          {/* <!-- Register buttons --> */}
+          <div class="text-center">
+            <p>Not a member? <a href="#!">Register</a></p>
+            {/* <p>or sign up with:</p>
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-facebook-f"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-google"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-twitter"></i>
+            </button>
+
+            <button type="button" class="btn btn-primary btn-floating mx-1">
+              <i class="fab fa-github"></i>
+            </button> */}
+          </div>
+        </form>
       <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
+
+      {/* <!-- Submit button --> */}
+      <button onClick={login} class="btn btn-success btn-block mb-4">Log in</button>
     </div>
   );
 }
