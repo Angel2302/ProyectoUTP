5a5b9ab889dbb9fe8e7000b5c73a9d11a1cb4b59
szapatao <szapatao@unal.edu.co>
2021-07-29 21:04:12 -0500

    Forgot passwors form improved

--- src/Forgot.js
+++ src/Forgot.js
@@ -32,28 +32,36 @@ const Forgot = () => {
     }
 
     return (
-        <div class="m-4">
-        <Fragment>
-            <h3>Password recovery will be sent to your email</h3>
-            <form class='container' onSubmit={sendDatos}>
-                <div class='row'>
-                    <input
-                            placeholder="Email"
-                            className="form-control"
-                            type="email"
-                            Email="Email"
-                            omChange={handleInputChange}
-                        ></input>
-                </div>            
-                <div class='mt-3'>
-                    <button class="btn btn-success regular-button">
-                    <Link to="/login" class= "nav-link text-light" >Recover password</Link>
-                    </button>
+        
+        <div>
+
+            <div class="container padding-bottom-3x mb-2 mt-5">
+            <Fragment>
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-md-10">
+                            <div class="forgot">
+                                <h2>Forgot your password?</h2>
+                                <p>Change your password in three easy steps. This will help you to secure your password!</p>
+                                <ol class="list-unstyled">
+                                    <li><span class="text-primary text-medium">1. </span>Enter your email address below.</li>
+                                    <li><span class="text-primary text-medium">2. </span>Our system will send you a temporary link</li>
+                                    <li><span class="text-primary text-medium">3. </span>Use the link to reset your password</li>
+                                </ol>
+                            </div>
+                            <form class="card mt-4 " onSubmit={sendDatos}>
+                                <div class="card-body">
+                                    <div class="form-group"> <label for="email-for-pass">Enter your email address</label> <input class="form-control" type="text" id="email-for-pass" required=""/> <small class="form-text text-muted">Enter the email address you used during the registration on TheGreenList.com. Then we'll email a link to this address.</small> </div>
+                                </div>
+                                <div class="card-footer"> <button class="btn btn-success" type="submit"><Link to="/login" class= "text-light" >Recover password</Link></button> <button class="btn btn-danger" type="submit">Back to Login</button> </div>
+                            </form>
+                        </div>
+                    </div>
+                </Fragment>
                 </div>
 
-            </form>
-        </Fragment>
-        </div>
+
+                
+        </div>       
     );
 }
  export default Forgot;
