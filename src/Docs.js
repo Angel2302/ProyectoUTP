7bc7f224c690c64577268f9bd968b2c76c889b9e
berperez2003 <berperez2003@yahoo.com.ar>
2021-08-09 17:21:49 -0500

    modifique el parrafo

--- src/Docs.js
+++ src/Docs.js
@@ -9,7 +9,9 @@ const Docs = () => {
             <p>
                 <h3>Gardening</h3>
                 Gardening is the art, technique and practice of cultivating gardens.
-                It consists of cultivating, both in an open and closed space (flowerbeds), flowers, trees, vegetables, or vegetables (orchards), either for aesthetics, for taste or for food, and in whose achievement the economic objective is secondary.
+                It consists of cultivating, both in an open and closed space (flowerbeds), 
+                flowers, trees, vegetables, or vegetables (orchards), either for aesthetics, 
+                for taste or for food, and in whose achievement the economic objective is secondary.
             </p>
         </div>
         <div class='col-auto'>
@@ -17,8 +19,9 @@ const Docs = () => {
                 <h3>Horticulture</h3>
                 Horticulture is the science, technology and business
                 involved in the production of vegetables for consumption.
-                 Horticulture is the technique of growing plants that grow in orchards.
-                The term comes etymologically from the Latin words hortus (‘garden’, ‘garden’, ‘plant’) and culture (‘cultivation’), that is, ‘cultivation in orchards’.
+                Horticulture is the technique of growing plants that grow in orchards.
+                The term comes etymologically from the Latin words hortus (‘garden’, ‘garden’, ‘plant’) 
+                and culture (‘cultivation’), that is, ‘cultivation in orchards’.
             </p>
         </div>
       
